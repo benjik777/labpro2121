e11fe61dd33390a0be0cf36017f9cad722b36bcc
Benoit Njika <P12210713@GH584-007.lec-admin.dmu.ac.uk>
2014-01-16 11:35:45 +0000

    now including forgotten.txt

--- sumxy.c
+++ sumxy.c
@@ -4,6 +4,7 @@ int main()
 {
     int x = 7;
     int y = 3;
+    int z = 5;
     printf ("%i + %i = %i\n", x, y, x+y);
     printf ("%i - %i = %i\n", x, y, x-y);
     printf ("%i / %i = %i\n", x, y, x/y);

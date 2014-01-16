f69032ea049ad046a70b2ea37a98b35cc2fad1f1
Benoit Njika <P12210713@GH584-007.lec-admin.dmu.ac.uk>
2014-01-16 11:28:42 +0000

    Test coding

--- sumxy.c
+++ sumxy.c
@@ -5,5 +5,7 @@ int main()
     int x = 7;
     int y = 3;
     printf ("%i + %i = %i\n", x, y, x+y);
+    printf ("%i - %i = %i\n", x, y, x-y);
+    printf ("%i / %i = %i\n", x, y, x/y);
     return 0;
 }

995514bd901529187b76ee7194b490ed83591503
Benoit Njika <P12210713@GH680-013.lec-admin.dmu.ac.uk>
2014-01-29 09:57:29 +0000

    Added maths function

---
+++ maths_branching.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 7;
+    int y = 3;
+    int z = 5;
+    printf ("%i + %i = %i\n", x, y, x+y);
+    printf ("%i - %i = %i\n", x, y, x-y);
+    printf ("%i / %i = %i\n", x, y, x/y);
+    return 0;
+}
